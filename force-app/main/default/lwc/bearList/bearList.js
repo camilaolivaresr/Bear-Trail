e2ad8681da18721abda67c0464dc97ff9b0cd0c3
Camila Olivares Rivera <colivarr@emeal.nttdata.com>
2026-07-14 17:12:28 -0400

    add style css file

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,3 +1,24 @@
 import { LightningElement } from 'lwc';
+import ursusResources from '@salesforce/resourceUrl/ursus_park';
+/** BearController.getAllBears() Apex method */
 
-export default class BearList extends LightningElement {}
+import getAllBears from '@salesforce/apex/BearController.getAllBears';
+export default class BearList extends LightningElement {
+	bears;
+	error;
+	appResources = {
+		bearSilhouette: `${ursusResources}/standing-bear-silhouette.png`,
+	};
+	connectedCallback() {
+		this.loadBears();
+	}
+	loadBears() {
+		getAllBears()
+			.then(result => {
+				this.bears = result;
+			})
+			.catch(error => {
+				this.error = error;
+			});
+	}
+}
